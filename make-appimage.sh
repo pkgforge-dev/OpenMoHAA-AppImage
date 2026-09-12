c176f6ce91434e97a823bac8a1645205a2e3e973
Link4Electronics <link4electronics@gmail.com>
2026-09-11 21:40:33 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -12,9 +11,7 @@ export ICON=PATH_OR_URL_TO_ICON
 export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/openmohaa
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
