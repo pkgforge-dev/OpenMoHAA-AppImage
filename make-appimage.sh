2f907ba576c24b49d31c1a1e99b3cde236f0cfc3
Link4Electronics <link4electronics@gmail.com>
2026-09-11 21:56:20 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,6 +9,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/symbolic/apps/org.openmoh.openmohaa.svg
 export DESKTOP=/usr/share/applications/org.openmoh.openmohaa.desktop
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/lib/openmohaa /usr/lib/libopenal.so*
